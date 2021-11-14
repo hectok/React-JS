4b365d2bc37905c1788b767fa966ebe6b83c63aa
Hector <hector.ledo.doval@gmail.com>
2021-11-14 12:18:15 +0100

    Ejercicio 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 //import Articulo from "./Components/Articulo";
 //import Formulario from "./Components/Formulario";
 //import Teclado from "./Components/Teclado";
-import ComponenteEventos from "./Components/ComponenteEventos";
-import MouseColor from "./Components/MouseColor/MouseColor";
+//import ComponenteEventos from "./Components/ComponenteEventos";
+//import MouseColor from "./Components/MouseColor/MouseColor";
+import Mensajes from "./Views/Mensajes/Mensajes.view";
+import './App.scss';
+
 function App() {
   return (
-    <>
-      <ComponenteEventos/>
-      <MouseColor/>
-    </>
+    <div className="App">
+ 
+      <div className="App-body">
+        <Mensajes/>
+      </div>     
+    </div>
   );
 }
 
