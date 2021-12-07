a1c3b5b73e6ce74f4cfe1666dde3f66622e80a1a
Hector <hector.ledo.doval@gmail.com>
2021-12-07 08:17:11 +0100

    Restoring files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import About from './pages/About/About.page.jsx';
-import Store from './pages/Store/Store.page.jsx';
-import Login from "./pages/Login/Login.page.jsx";
-import Product from './pages/Producto/Producto.page';
-import Nav from './Components/Nav/Nav.component.jsx';
-import { ProtectedRoute } from './Components/ProtectedRoute/ProtectedRoute.component.jsx';
+import Mensajes from "./Views/Mensajes/Mensajes.view";
 import './App.scss';
 
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Switch>
-            <ProtectedRoute exact path="/">
-              <Login />
-            </ProtectedRoute >
-            <ProtectedRoute exact path="/" element={<Store/>} />
-            <ProtectedRoute exact path="/about" component={About} />
-            <ProtectedRoute path="/product/:id" component={Product} />
-          </Switch>
-        </div>
-      </Router>
+
+      <div className="App-body">
+        <Mensajes />
+      </div>
     </div>
   );
 }
+
+export default App;
