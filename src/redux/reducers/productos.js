0c0be9737dbe67a92b832ece9a723459178b042d
Hector <hector.ledo.doval@gmail.com>
2021-12-09 08:21:50 +0100

    Wip

---
+++ src/redux/reducers/productos.js
@@ -0,0 +1,24 @@
+import { ACTIONS_PRODUCTOS } from '../actions/productos';
+
+const initialState = {
+    productos: []
+};
+
+export const productos = (state = initialState.productos, action) => {
+    switch (action.type) {
+        case ACTIONS_PRODUCTOS.VACIAR:
+            return [];
+        case ACTIONS_PRODUCTOS.CREAR:
+            state.push(action.payload);
+
+            return [...state];
+        case ACTIONS_PRODUCTOS.LEER:
+            state[action.payload].leido = !state[action.payload].leido;
+
+            return [...state];
+        default:
+            return [...state];
+    }
+}
+
+export default productos;
